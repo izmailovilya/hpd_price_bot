6fbfdeda169757dd03cd6f955dc808a2c617094d
valmerbo <voliamz.i.lya@gmail.com>
2022-12-30 01:34:22 +0700

    channel_rates

--- rates.py
+++ rates.py
@@ -2,22 +2,22 @@ import requests
 import json
 
 def get_usdt_rate():
-    data = {"proMerchantAds":False,"page":1,"rows":10,"payTypes":["RosBankNew"],"countries":[],"publisherType":"merchant","asset":"USDT","fiat":"RUB","tradeType":"BUY","transAmount":"200000"} 
+    data = {"proMerchantAds":False,"page":1,"rows":10,"payTypes":["RosBankNew"],"countries":[],"publisherType":"merchant","asset":"USDT","fiat":"RUB","tradeType":"BUY","transAmount":"100000"} 
     r = requests.post('https://p2p.binance.com/bapi/c2c/v2/friendly/c2c/adv/search', json=data)
-    o = r.json()
+    o = r.json()["data"][:3]
     sum = 0
-    for result in o["data"]:
+    for result in o:
         sum = sum + float(result["adv"]["price"])
-    return (sum / 10) + 1
+    return (sum / 3) + 1
 
 def get_currency_rate(currency):
     data = {"proMerchantAds":False,"page":1,"rows":10,"payTypes":[],"countries":[],"publisherType":"merchant","asset":"USDT","fiat":currency.upper(),"tradeType":"SELL"} 
     r = requests.post('https://p2p.binance.com/bapi/c2c/v2/friendly/c2c/adv/search', json=data)
-    o = r.json()
+    o = r.json()["data"][:3]
     sum = 0
-    for result in o["data"]:
+    for result in o:
         sum += float(result["adv"]["price"])
-    return sum / 10
+    return sum / 3
 
 def get_usdt_currency(currency):
     return get_usdt_rate()/get_currency_rate(currency) * 1.05
